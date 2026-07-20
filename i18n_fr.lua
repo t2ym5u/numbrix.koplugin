ac157c0ba29089be096a154f0044fcdb747ffbb4
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-20 14:18:38 +0200

    i18n: add missing FR translations

---
+++ i18n_fr.lua
@@ -0,0 +1,5 @@
+return {
+    ["%1\xC3\x97%2 (1\xE2\x80\x93%3) \xC2\xB7 %4 \xC2\xB7 Empty: %5"] = { fr = "%1×%2 (1–%3) · %4 · Vides : %5" },
+    ["Numbrix"]                       = { fr = "Numbrix" },
+    ["Snake-like number path puzzle"] = { fr = "Casse-tête de chemin numérique façon serpent" },
+}
