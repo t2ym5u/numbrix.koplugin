e5579a7bb7c1b0e84cebc5d61c55a49a8099412b
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-28 13:31:48 +0200

    chore: bump version to 1.1.6

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name        = "numbrix",
     fullname    = _("Numbrix"),
     description = _("Snake-like number path puzzle"),
-    version     = "1.1.5",
+    version     = "1.1.6",
 }
