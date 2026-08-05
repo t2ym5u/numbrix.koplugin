ed1f2fb4a2a89574744751614c949fa0ef1397c1
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-05 14:59:43 +0200

    feat: add ES and DE translations

--- i18n_fr.lua
+++ i18n_fr.lua
@@ -1,5 +1,5 @@
 return {
-    ["%1\xC3\x97%2 (1\xE2\x80\x93%3) \xC2\xB7 %4 \xC2\xB7 Empty: %5"] = { fr = "%1×%2 (1–%3) · %4 · Vides : %5" },
-    ["Numbrix"]                       = { fr = "Numbrix" },
-    ["Snake-like number path puzzle"] = { fr = "Casse-tête de chemin numérique façon serpent" },
+    ["%1\xC3\x97%2 (1\xE2\x80\x93%3) \xC2\xB7 %4 \xC2\xB7 Empty: %5"] = { fr = "%1×%2 (1–%3) · %4 · Vides : %5", es = "%1\xC3\x97%2 (1\xE2\x80\x93%3) \xC2\xB7 %4 \xC2\xB7 Vacías: %5", de = "%1\xC3\x97%2 (1\xE2\x80\x93%3) \xC2\xB7 %4 \xC2\xB7 Leer: %5" },
+    ["Numbrix"]                       = { fr = "Numbrix", es = "Numbrix", de = "Numbrix" },
+    ["Snake-like number path puzzle"] = { fr = "Casse-tête de chemin numérique façon serpent", es = "Puzle de camino numérico tipo serpiente", de = "Zahlenpfad-Rätsel im Schlangenstil" },
 }
